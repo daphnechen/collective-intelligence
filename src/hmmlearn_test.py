e63756b6823a8aead1398edd108d2aba73f5a3bf
Daphne Chen <daphne.chen96@gmail.com>
2022-07-01 11:18:53 -0400

    latest working version, need to fix bug with changing number of hidden states

--- src/hmmlearn_test.py
+++ src/hmmlearn_test.py
@@ -10,11 +10,7 @@ import seaborn as sns
 sns.set_style('darkgrid')
 
 data = np.load('salad_data_synthetic.npy', allow_pickle=True)
-# pprint(data)
 data = np.reshape(data,[len(data),1])
-
-# pprint(len(data))
-# print(data.shape)
 
 # hidden_states = []
 # for i in range(len(data)):
@@ -30,12 +26,14 @@ for i in range(len(data)):
     
 # formatted_data = np.array(formatted_data)
 # pprint(formatted_data)
-padded_formatted_data = np.array(list(zip_longest(*formatted_data, fillvalue=0))).T # since fit() only takes arrays of the same length
-padded_formatted_data_reshaped = np.reshape(padded_formatted_data, (-1, 1))
-print(padded_formatted_data_reshaped.shape, 'padded formatted data reshaped shape lmao')
+padded_formatted_data = np.array(list(zip_longest(*formatted_data, fillvalue=0))).T # since hmm.fit() only takes arrays of the same length
+# padded_formatted_data_reshaped = np.reshape(padded_formatted_data, (-1, 1)) # fit data to required dims for hmm.fit()
+pprint(padded_formatted_data)
+print('padded_formatted_data shape: \n', padded_formatted_data.shape)
 
-model = hmm.GaussianHMM(n_components=20, n_iter=100).fit(padded_formatted_data_reshaped) # TODO change number of N
-hidden_states = model.predict(padded_formatted_data_reshaped)
+# param = set(padded_formatted_data.ravel())
+model = hmm.GaussianHMM(n_components=10, n_iter=100).fit(padded_formatted_data) # TODO change number of N
+hidden_states = model.predict(padded_formatted_data)
 print(len(hidden_states), 'hidden states length')
 pprint(hidden_states)
 
@@ -51,15 +49,15 @@ def get_clusters(hidden_seqs):
     return cluster_labels, cluster_means
 
 labels, means = get_clusters(hidden_states)
-print(labels)
-print(means)
+print('labels: \n', labels)
+print('means: \n', means)
 
 # df = pd.read_csv('new_data.csv')
 # g = sns.PairGrid(df)
 # g.map(sns.scatterplot)
 
 ## visualize the clusters
-plt.scatter(padded_formatted_data_reshaped, padded_formatted_data_reshaped, label="50 salads synthetic data", c=labels, cmap='rainbow') # c=labels/242,
+plt.scatter(range(len(hidden_states)), hidden_states, label="50 salads synthetic data", c=labels, cmap='rainbow')
 # plt.scatter(padded_formatted_data_reshaped[:,0], padded_formatted_data_reshaped[:,1], label="50 salads synthetic data", c=labels, cmap='rainbow') # c=labels/242,
-# plt.scatter(padded_formatted_data[0,:], padded_formatted_data[1,:], c=labels)
+
 plt.show()
