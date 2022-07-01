29867e2376e73d97c4c34c16128d4bdec56865d5
Daphne Chen <daphne.chen96@gmail.com>
2022-07-01 19:47:49 -0400

    fixed model.fit() where it was originally taking in the wrong/old data

--- src/hmmlearn_test.py
+++ src/hmmlearn_test.py
@@ -11,45 +11,32 @@ from nltk import ngrams
 
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import cm
-import seaborn as sns
-sns.set_style('darkgrid')
 
+
+# load the data
 data = np.load('salad_data_synthetic.npy', allow_pickle=True)
 data = np.reshape(data,[len(data),1])
 
-# hidden_states = []
-# for i in range(len(data)):
-#     print(data[i][0])
-#     model = hmm.GaussianHMM(n_components=2, n_iter=100).fit(data[i][0])
-
-#     hidden_state = model.predict(data)
-#     hidden_states.append(hidden_state)
-
+# format data from npy
 formatted_data = []
 lengths = []
 for i in range(len(data)):
     formatted_data.append(data[i][0])
     lengths.append(len(data[i][0]))
-# print('length of lengths arr: \n', len(lengths))
-    
-# formatted_data = np.array(formatted_data)
-# pprint(formatted_data)
-padded_formatted_data = np.array(list(zip_longest(*formatted_data, fillvalue=0))).T # since hmm.fit() only takes arrays of the same length
-padded_formatted_data_reshaped = np.reshape(padded_formatted_data, (-1, 1)) # fit data to required dims for hmm.fit()
-# pprint(padded_formatted_data)
-# print('padded_formatted_data shape: \n', padded_formatted_data.shape)
 
-# param = set(padded_formatted_data.ravel())
-model = hmm.GaussianHMM(n_components=10, n_iter=100).fit(padded_formatted_data_reshaped, lengths=lengths) # TODO change number of N
-# hidden_states = model.predict(padded_formatted_data_reshaped, lengths=lengths)
-h_s = [model.predict(np.array(element).reshape(-1, 1)) for element in formatted_data]
-print(len(h_s), 'hidden states length')
-# print(h_s.shape, 'hidden states shape')
-print('HIDDEN STATES: \n', h_s)
+# train an HMM and get the hidden states
+model = hmm.GaussianHMM(n_components=10, n_iter=100)  # TODO change number of N
+model.fit(np.concatenate(formatted_data).reshape(-1, 1), lengths=lengths)
+hidden_states = [model.predict(np.array(element).reshape(-1, 1)) for element in formatted_data]
+print(len(hidden_states), 'hidden states length')
+print('HIDDEN STATES: \n', hidden_states)
 
 
-# run clustering
 def get_clusters(hidden_seqs):
+    '''
+        Get kmeans clusters
+        Only works for sequences of equal length
+    '''
     features = []
     if type(hidden_seqs) is list:
         features = np.reshape(hidden_seqs, (-1, 1))
@@ -65,6 +52,7 @@ def get_clusters(hidden_seqs):
 
 def get_clusters_as_ngrams(hidden_seqs):
     '''
+        Get kmeans clusters
         Account for sequences of different length
     '''
     vocabulary = list()
@@ -83,13 +71,10 @@ def get_clusters_as_ngrams(hidden_seqs):
     return cluster_labels, cluster_means
 
 
-labels, means = get_clusters_as_ngrams(h_s)
+# run clustering
+labels, means = get_clusters_as_ngrams(hidden_states)
 print('labels: \n', labels)
 print('means: \n', means)
-
-# df = pd.read_csv('new_data.csv')
-# g = sns.PairGrid(df)
-# g.map(sns.scatterplot)
 
 # generate samples? try and plot these
 # x, z = model.sample(500)
@@ -108,16 +93,14 @@ print('means: \n', means)
 ## visualize the clusters
 # plt.scatter(range(len(hidden_states)), hidden_states, label="50 salads synthetic data", c=labels, cmap='rainbow')
 
-fig, ax = plt.subplots() # len(h_s), 10
-n = len(h_s)-535
+fig, ax = plt.subplots()
+n = len(hidden_states)-535
 color = iter(cm.rainbow(np.linspace(0, 1, n)))
-for i in range(len(h_s)-535):
-    seq = h_s[i].tolist()
+for i in range(len(hidden_states)-535):
+    seq = hidden_states[i].tolist()
     label=labels[i]
-    # print('seq: \n', seq)
-    # print('label: \n', label)
     c = next(color)
-    ax.plot(seq, label="label: "+str(label), c=c) # label="50 salads synthetic data", c=label
+    ax.plot(seq, label="label: "+str(label), c=c)
 
 ax.legend(loc='best')
 ax.set_xlabel('step in sequence')
